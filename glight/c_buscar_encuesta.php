5b53a2bc9784cc66528326afcc8245ac0b0fc6be
Antony <ajhenaor@unal.edu.co>
2015-11-17 21:27:47 -0500

    buscar_encuesta con seguridad

--- glight/c_buscar_encuesta.php
+++ glight/c_buscar_encuesta.php
@@ -7,12 +7,12 @@
 			if(strcmp($msg_type, "Registro completado")==0){
 				$this->engine->assign('red',1);
 				$msg_icon="check-square";
-				$msg_dir=$gvar['l_global']."login.php";
 			}else{
 				$msg_icon="warning";
 				$msg_dir="";
 			}
 
+			$msg_dir=$gvar['l_global']."login.php";
 			$this->temp_aux = 'message.tpl';
 			$this->engine->assign('object',$this->post);
 			$this->engine->assign('msg_icon', $msg_icon);
@@ -33,26 +33,33 @@
 		}
 
 	    public function display(){
-	    	if(!isset($_POST['consulta'])){
-	    		$this->orm->connect();
-	    		$options['interes']['lvl2']="by_persona";
-	    		$cod['interes']['persona']=$_SESSION['persona']['cedula'];
-	    		$this->orm->read_data(array("interes"), $options, $cod);
-	    		$interes = $this->orm->get_objects("interes",$components);
-	    		
-	    		$options['encuesta']['lvl2']="by_tipo";
-	    		$cod['encuesta']['tipo']=$interes;
-				$this->orm->read_data(array("encuesta"), $options, $cod);
-				$encuestas= $this->orm->get_objects("encuesta");
-				$this->engine->assign('encuestas', $encuestas);
+	    	if($_SESSION['persona']['rol']=='usuario'){
+			    if(!isset($_POST['consulta'])){
+			    		$this->orm->connect();
+			    		$options['interes']['lvl2']="by_persona";
+			    		$cod['interes']['persona']=$_SESSION['persona']['cedula'];
+			    		$this->orm->read_data(array("interes"), $options, $cod);
+			    		$interes = $this->orm->get_objects("interes",$components);
+			    		
+			    		$options['encuesta']['lvl2']="by_tipo";
+			    		$cod['encuesta']['tipo']=$interes;
+						$this->orm->read_data(array("encuesta"), $options, $cod);
+						$encuestas= $this->orm->get_objects("encuesta");
+						$this->engine->assign('encuestas', $encuestas);
+			    	}
+
+			}else{
+	    		 $this->displayMessage('Acción no permitida','Usted no tiene permisos para realizar esta acción');
 	    	}
 
-	    		$this->engine->assign('title', "Buscar encuesta");
-	    		$this->engine->display('header.tpl');
-	    		$this->engine->display($this->temp_aux);
-		        $this->engine->display('buscar_encuesta.tpl');
-		        $this->engine->display('footer.tpl');
+	    	$this->engine->assign('title', "Buscar encuesta");
+			$this->engine->display('header.tpl');
+			$this->engine->display($this->temp_aux);
+			$this->engine->display('buscar_encuesta.tpl');
+			$this->engine->display('footer.tpl');
 	    }
+
+
 		
 		public function run(){
 			try {if (isset($this->get->option)){$this->{$this->get->option}();}}
